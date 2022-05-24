cfbdbdfdc856553af97e2a39c0459596d9dfa5fb
Gian-Laager <gianlaager114@gmail.com>
2022-05-24 13:55:13 +0200

    added h bar

---
+++ src/sa/constants.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace std {
+    namespace numbers {
+        constexpr double h_bar = 1.0546e-34;
+    }
+}
--- src/sa/pch.h
+++ src/sa/pch.h
@@ -1,5 +1,6 @@
 #pragma once
 
 #include <iostream>
+#include <numbers>
 
 int add(int a, int b);
