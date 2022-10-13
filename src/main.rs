e5a9ba217deffd3cd1a5fd2ece91a4fa221bdf38
Gian-Laager <gian.laager@source-graphics.ch>
2022-10-13 14:07:21 +0200

    plot parts seperatly

--- src/main.rs
+++ src/main.rs
@@ -52,58 +52,132 @@ fn main() {
     let output_dir = Path::new("output");
     std::env::set_current_dir(&output_dir).unwrap();
 
-    let wave_function = wave_function_builder::SuperPosition::new(
-        &potentials::double_mexican_hat,
-        MASS,
-        &[
-            (2, 1.0.into()),
-            (5, 1.0.into()),
-            (8, 1.0.into()),
-            (40, 1.0.into()),
-        ],
-        APPROX_INF,
-        VIEW_FACTOR,
-        ScalingType::Renormalize(complex(1.0, 0.0)),
-    );
-
-    // let wave_function = wave_function_builder::WaveFunction::new(
+    // let wave_function = wave_function_builder::SuperPosition::new(
     //     &potentials::mexican_hat,
     //     MASS,
-    //     2,
+    //     &[
+    //         // (5, 1.0.into()),
+    //         (12, 1.0.into()),
+    //         // (21, 1.0.into()),
+    //     ],
     //     APPROX_INF,
-    //     1.5,
-    //     ScalingType::Renormalize(1.0.into()),
+    //     VIEW_FACTOR,
+    //     ScalingType::Renormalize(complex(1.0, 0.0)),
     // );
-    // println!("energy: {}", wave_function.get_energy());
-    
-    let all_values = evaluate_function_between(
-        &wave_function,
-        wave_function.get_view().0,
-        wave_function.get_view().1,
-        NUMBER_OF_POINTS,
+
+    let wave_function = wave_function_builder::WaveFunction::new(
+        &potentials::square,
+        MASS,
+        12,
+        APPROX_INF,
+        1.5,
+        ScalingType::Renormalize(1.0.into()),
     );
-    
-    let all_values_str = all_values
+    println!("energy: {}", wave_function.get_energy());
+
+    let wkb_values = wave_function
+        .get_wkb_ranges()
+        .iter()
+        .map(|range| {
+            evaluate_function_between(
+                &wave_function,
+                f64::max(wave_function.get_view().0, range.0),
+                f64::min(wave_function.get_view().1, range.1),
+                NUMBER_OF_POINTS,
+            )
+        })
+        .collect::<Vec<Vec<Point<f64, Complex64>>>>();
+
+    let airy_values = wave_function
+        .get_airy_ranges()
+        .iter()
+        .map(|range| {
+            evaluate_function_between(
+                &wave_function,
+                f64::max(wave_function.get_view().0, range.0),
+                f64::min(wave_function.get_view().1, range.1),
+                NUMBER_OF_POINTS,
+            )
+        })
+        .collect::<Vec<Vec<Point<f64, Complex64>>>>();
+
+    let wkb_values_str = wkb_values
         .par_iter()
-        .map(|p| -> String { format!("{} {} {}\n", p.x, p.y.re, p.y.im) })
-        .reduce(|| String::new(), |s: String, current: String| s + &*current);
-    
+        .map(|values| {
+            values
+                .par_iter()
+                .map(|p| -> String { format!("{} {} {}\n", p.x, p.y.re, p.y.im) })
+                .reduce(|| String::new(), |s: String, current: String| s + &*current)
+        })
+        .reduce(
+            || String::new(),
+            |s: String, current: String| s + "\n\n" + &*current,
+        );
+
+    let airy_values_str = airy_values
+        .par_iter()
+        .map(|values| {
+            values
+                .par_iter()
+                .map(|p| -> String { format!("{} {} {}\n", p.x, p.y.re, p.y.im) })
+                .reduce(|| String::new(), |s: String, current: String| s + &*current)
+        })
+        .reduce(
+            || String::new(),
+            |s: String, current: String| s + "\n\n" + &*current,
+        );
+
     let mut data_full = File::create("data.txt").unwrap();
-    data_full.write_all(all_values_str.as_ref()).unwrap();
-    
+    data_full.write_all(wkb_values_str.as_ref()).unwrap();
+    data_full.write_all("\n\n".as_bytes()).unwrap();
+    data_full.write_all(airy_values_str.as_ref()).unwrap();
+
     let mut plot_3d_file = File::create("plot_3d.gnuplot").unwrap();
-    let plot_3d_cmd: String = "splot \"data.txt\" u 1:2:3 t \"Psi\" w l".to_string();
+
+    let wkb_3d_cmd = (1..=wkb_values.len())
+        .into_iter()
+        .map(|n| format!("\"data.txt\" u 1:2:3 i {} t \"WKB {}\" w l", n - 1, n))
+        .collect::<Vec<String>>()
+        .join(", ");
+
+    let airy_3d_cmd = (1..=airy_values.len())
+        .into_iter()
+        .map(|n| format!("\"data.txt\" u 1:2:3 i {} t \"Airy {}\" w l", n + wkb_values.len() - 1, n))
+        .collect::<Vec<String>>()
+        .join(", ");
+    let plot_3d_cmd: String = "splot ".to_string() + &wkb_3d_cmd + ", " + &airy_3d_cmd;
     plot_3d_file.write_all(plot_3d_cmd.as_ref()).unwrap();
-    
+
     let mut plot_file = File::create("plot.gnuplot").unwrap();
-    
-    let plot_cmd = "plot \"data.txt\" u 1:2 t \"Psi\" w l".to_string();
-    
+    let wkb_cmd = (1..=wkb_values.len())
+        .into_iter()
+        .map(|n| format!("\"data.txt\" u 1:2 i {} t \"WKB {}\" w l", n - 1, n))
+        .collect::<Vec<String>>()
+        .join(", ");
+
+    let airy_cmd = (1..=airy_values.len())
+        .into_iter()
+        .map(|n| format!("\"data.txt\" u 1:2 i {} t \"Airy {}\" w l", n + wkb_values.len() - 1, n))
+        .collect::<Vec<String>>()
+        .join(", ");
+    let plot_cmd: String = "plot ".to_string() + &wkb_cmd + ", " + &airy_cmd;
+
     plot_file.write_all(plot_cmd.as_ref()).unwrap();
-    
+
     let mut plot_imag_file = File::create("plot_im.gnuplot").unwrap();
-    
-    let plot_imag_cmd = "plot \"data.txt\" u 1:3 t \"Psi\" w l".to_string();
-    
+
+    let wkb_im_cmd = (1..=wkb_values.len())
+        .into_iter()
+        .map(|n| format!("\"data.txt\" u 1:3 i {} t \"WKB {}\" w l", n - 1, n))
+        .collect::<Vec<String>>()
+        .join(", ");
+
+    let airy_im_cmd = (1..=airy_values.len())
+        .into_iter()
+        .map(|n| format!("\"data.txt\" u 1:3 i {} t \"Airy {}\" w l", n + wkb_values.len() - 1, n))
+        .collect::<Vec<String>>()
+        .join(", ");
+    let plot_imag_cmd: String = "plot ".to_string() + &wkb_im_cmd + ", " + &airy_im_cmd;
+
     plot_imag_file.write_all(plot_imag_cmd.as_ref()).unwrap();
 }
