542ed34ff225818fd8ab109dc8fef8a903bc9127
Gian-Laager <gian.laager@source-graphics.ch>
2022-11-22 23:38:13 +0100

    removed pot from main

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 mod airy;
 mod airy_wave_func;
+mod benchmarks;
 mod check;
 mod energy;
 mod integrals;
@@ -14,7 +15,6 @@ mod turning_points;
 mod utils;
 mod wave_function_builder;
 mod wkb_wave_func;
-mod benchmarks;
 
 use crate::airy::airy_ai;
 use crate::airy_wave_func::AiryWaveFunction;
@@ -45,12 +45,6 @@ const VALIDITY_LL_FACTOR: f64 = 3.5;
 const APPROX_INF: (f64, f64) = (-200.0, 200.0);
 const VIEW_FACTOR: f64 = 0.5;
 
-fn pot(x: f64) -> f64 {
-    const SIGMA: f64 = 0.01;
-    const EPSILON: f64 = 1.0;
-    4.0 * EPSILON * ((SIGMA / x).powi(12) - (SIGMA / x).powi(6))
-}
-
 fn main() {
     // let wave_function = wave_function_builder::WaveFunction::new(
     //     &potentials::mexican_hat,
@@ -60,7 +54,7 @@ fn main() {
     //     VIEW_FACTOR,
     //     ScalingType::Renormalize(complex(0.0, f64::consts::PI / 4.0).exp()),
     // );
-
+    
     let wave_function = wave_function_builder::SuperPosition::new(
         &potentials::square,
         1.0, // mass
@@ -79,14 +73,14 @@ fn main() {
         VIEW_FACTOR,
         ScalingType::Renormalize(complex(1.0, 0.0)),
     );
-
+    
     let output_dir = Path::new("output");
-
+    
     // For WaveFunction
     // plot::plot_wavefunction(&wave_function, output_dir, "data.txt");
     // plot::plot_wavefunction_parts(&wave_function, output_dir, "data.txt");
     // plot::plot_probability(&wave_function, output_dir, "data.txt");
-
+    
     // For SuperPosition
     plot::plot_superposition(&wave_function, output_dir, "data.txt");
     // plot::plot_probability_superposition(&wave_function, output_dir, "data.txt");
